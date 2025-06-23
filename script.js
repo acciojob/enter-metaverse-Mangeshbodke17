21357aefb20720dc4925f7c5c79bf3c60f030a62
Mangesh Bodke <mangeshbodke2025@gmail.com>
2025-06-23 12:33:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+
+
+let btn=document.querySelector("button");
+
+
+
+btn.addEventListener("click",()=>{
+
+
+    
+let p=document.querySelector("p");
+
+
+
+let h1=document.createElement("h1");
+h1.innerText="Enter the Metaverse";
+
+
+
+
+  p.replaceWith(h1);
+
+
+})
