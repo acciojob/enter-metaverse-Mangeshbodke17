af737b98ee54c562828c8beebaa05454bede9a2b
Mangesh Bodke <mangeshbodke2025@gmail.com>
2025-06-23 12:53:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-//your JS code here. If required.
+let btn = document.querySelector("#enterBtn");
 
+btn.addEventListener("click", () => {
+  let p = document.querySelector("#status");
 
-let btn=document.querySelector("#enterBtn");
-
-
-btn.addEventListener("click",()=>{
-
-let p=document.querySelector("#status");
-
-let h1=document.createElement("h1");
-h1.innerText="Entered Metaverse";
+  let h1 = document.createElement("h1");
+  h1.innerText = "Entered Metaverse";
+  h1.id = "status";  // 🔥 This is the key change
 
   p.replaceWith(h1);
-
-
-})
+});
